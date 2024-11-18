02feae54b89fac1c737adb57a58d9ddff4bfbbf3
habbaza <habbazahubbal@gmail.com>
2024-11-18 13:01:08 +0700

    setting cors config

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const app = express();
 
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(express.json());
