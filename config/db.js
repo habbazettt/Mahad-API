bc53bab16421cbe8cee56f916ed5c6b908624d17
habbaza <habbazahubbal@gmail.com>
2024-12-01 16:08:46 +0700

    edit index.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://habbazahubbal:admin@mahad-tahfidz.nmzc1.mongodb.net/mahad?retryWrites=true&w=majority&appName=mahad-tahfidz'
+const url = process.env.MONGO_URI || 'mongodb+srv://habbazahubbal:admin@mahad-tahfidz.nmzc1.mongodb.net/mahad?retryWrites=true&w=majority&appName=mahad-tahfidz'
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(url);
-        console.log(`MongoDB Connected: ${conn.connection.host}`.blue.underline.bold);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const port = process.env.PORT;
 const connectDB = require("./config/db")
 
 const app = express();
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}))
 
 connectDB()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
 
 app.get("/", (req, res) => {
     res.send("API running")
@@ -23,5 +28,5 @@ app.use("/api/setoran", require("./routes/setoranRoutes"))
 app.use(errorHandler)
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`.bgBlue.underline.bold);
+    console.log(`Server running on port ${port}`);
 })
